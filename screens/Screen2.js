04807ea319ab6c562b9ebeac203ad1039ae9d226
monk <109079228+MrHid3@users.noreply.github.com>
2025-04-23 08:55:00 +0200

    lekcja

--- screens/Screen2.js
+++ screens/Screen2.js
@@ -3,8 +3,12 @@ import {useEffect, useState} from "react";
 import MyButton from "../components/MyButton";
 import {createNote, loadNotes, getKeys} from "./Screen1"
 import * as SecureStore from "expo-secure-store";
+import {Picker} from '@react-native-picker/picker'
 
 const Screen2 = ({navigation}) => {
+
+    const [categories, setCategories] = useState([]);
+
     async function getKeys(){
         const storedKeys = await SecureStore.getItemAsync("keys");
         if (!storedKeys) {
@@ -14,16 +18,16 @@ const Screen2 = ({navigation}) => {
         return JSON.parse(storedKeys);
     }
 
-    async function loadNotes() {
+    async function loadCategories() {
         const storedKeys = await getKeys();
-        const notesArray = [];
+        const categoriesArray = [];
         for (const key of storedKeys) {
             const content = await SecureStore.getItemAsync(key);
             if (content) {
-                notesArray.push({title: key, content});
+                categoriesArray.push(key);
             }
         }
-        setNotes(notesArray);
+        setCategories(categoriesArray);
     }
 
     async function createNote(title, content){
@@ -37,7 +41,6 @@ const Screen2 = ({navigation}) => {
         }else{
             const newKeys = [...currentKeys, title];
             await SecureStore.setItemAsync("keys", JSON.stringify(newKeys));
-            console.log( colors[Math.floor(Math.random * colors.length)])
             let note = {
                 title: title,
                 content: content,
@@ -47,16 +50,34 @@ const Screen2 = ({navigation}) => {
             await SecureStore.setItemAsync(title, JSON.stringify(note));
             navigation.navigate("s1");
         }
-        
     }
+
+    useEffect(() => {
+        const initialize = async () => {
+            await loadCategories();
+            const unsubscribe = navigation.addListener('focus', () => {
+                loadCategories();
+            })
+        };
+        initialize();
+        categories.map((category, i) => {
+            console.log(category)
+        })
+    }, [])
 
     const [title, setTitle] = useState("Title");
     const [content, setContent] = useState("Content");
+    const [category, setCategory] = useState(categories[0]);
     return(
         <View style={styles.main}>
             <View style={styles.container}>
                 <TextInput inputMode="text" style={styles.input} placeholder="Title" onChangeText={(text) => setTitle(text)} required ></TextInput>
                 <TextInput inputMode="text" style={styles.input} placeholder="Content" onChangeText={(text) => setContent(text)} required ></TextInput>
+                <Picker style={{width: 120}} mode="dropdown" onValueChange={(value) => setCategory(value)}>
+                    {categories.map((category, i) => (
+                            <Picker.Item key={i} label={category} value={category} style={{width: 80}}/>
+                    ))}
+                </Picker>
                 <MyButton text="Create Note" style={styles} onPress={async() => {await createNote(title, content)}}></MyButton>
             </View>
         </View>
